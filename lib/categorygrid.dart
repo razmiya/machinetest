d84a52a80abe369539f9fc13373a98a4b52e7226
razmiya <razmiyat@gmail.com>
2024-03-01 13:16:27 +0530

    machinetest with dtream builder

--- lib/categorygrid.dart
+++ lib/categorygrid.dart
@@ -49,7 +49,6 @@ class _CategoryGridState extends State<CategoryGrid> {
       description: "Lamb, hogget, and mutton are the meat of domestic sheep (species Ovis aries) at different ages.\r\n\r\nA sheep in its first year is called a lamb, and its meat is also called lamb. The meat of a juvenile sheep older than one year is hogget; outside the USA this is also a term for the living animal. The meat of an adult sheep is mutton, a term only used for the meat, not the living animals."
           " The term mutton is almost always used to refer to goat meat in the Indian subcontinent.\r\n\r\n",
     ),
-
     // Add more meals as needed
   ];
 
@@ -154,6 +153,7 @@ class _CategoryGridState extends State<CategoryGrid> {
     );
   }
 }
+
 class MealDetailPage extends StatelessWidget {
   final Meal meal;
 
